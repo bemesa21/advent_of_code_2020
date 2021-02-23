e2936f3a99165e57a0208859b2a70ea4c59a31a8
Berenice Medel <bemesa21@gmail.com>
2021-02-22 23:05:05 -0600

    add exercise 4 part one with test and task

--- lib/advent_of_code/day_04.ex
+++ lib/advent_of_code/day_04.ex
@@ -1,7 +1,44 @@
 defmodule AdventOfCode.Day04 do
   def part1(args) do
+    args
+    |> format_input()
+    |> Enum.map(&validate_required(&1))
+    |> Enum.count(fn x -> x == true end)
   end
 
   def part2(args) do
+  end
+
+  defp format_input(input) do
+    input
+    |> String.split("\n\n")
+    |> Enum.map(&to_map(&1))
+  end
+
+  defp to_map(params) do
+    params
+    |> String.trim()
+    |> String.split(["\n", " "])
+    |> Enum.reduce(%{}, fn field, result ->
+      [key, value] = String.split(field, [":", " "])
+      Map.put(result, key, value)
+    end)
+  end
+
+  defp validate_required(%{
+         "byr" => _,
+         "iyr" => _,
+         "eyr" => _,
+         "hgt" => _,
+         "hcl" => _,
+         "ecl" => _,
+         "pid" => _
+       }) do
+    true
+  end
+
+  defp validate_required(_), do: false
+
+  defp params_validations(string) do
   end
 end
--- test/advent_of_code/day_04_test.exs
+++ test/advent_of_code/day_04_test.exs
@@ -3,12 +3,14 @@ defmodule AdventOfCode.Day04Test do
 
   import AdventOfCode.Day04
 
-  @tag :skip
   test "part1" do
-    input = nil
-    result = part1(input)
+    input = "ecl:gry pid:860033327 eyr:2020 hcl:#fffffd\nbyr:1937 iyr:2017 cid:147 hgt:183cm\n\n
+    iyr:2013 ecl:amb cid:350 eyr:2023 pid:028048884\nhcl:#cfa07d byr:1929\n\n
+    hcl:#ae17e1 iyr:2013\neyr:2024\necl:brn pid:760753108 byr:1931\nhgt:179cm\n\n
+    hcl:#cfa07d eyr:2025 pid:166559648\niyr:2011 ecl:brn hgt:59in\n"
 
-    assert result
+    result = part1(input)
+    assert result == 2
   end
 
   @tag :skip
