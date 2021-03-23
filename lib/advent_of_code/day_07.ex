4fc0f182a7036d0de6a0012c847c92e85126d2c9
Berenice Medel <bemesa21@gmail.com>
2021-03-22 20:20:25 -0600

    read and format input [d7p1]

--- lib/advent_of_code/day_07.ex
+++ lib/advent_of_code/day_07.ex
@@ -4,4 +4,23 @@ defmodule AdventOfCode.Day07 do
 
   def part2(args) do
   end
+
+  def format_bag(content) do
+    [bag, content] =
+    content
+    |> String.replace(["bag", "bags"], "")
+    |> String.split("contain")
+
+    formatted_content =
+    Regex.scan(~r/[0-9]+[a-z\s]*/, content)
+    |> List.flatten()
+    |> Enum.reduce(%{}, &format_content/2)
+
+    %{String.trim(bag) => formatted_content}
+  end
+#iex(1)> input |> String.split("\n", trim: true) |> Enum.map(&AdventOfCode.Day07.format_bag/1)
+  def format_content(<<number::binary-size(1)>> <> color, content) do
+    trimmed_color = String.trim(color)
+    Map.put(content, trimmed_color, String.to_integer(number))
+  end
 end
--- test/advent_of_code/day_07_test.exs
+++ test/advent_of_code/day_07_test.exs
@@ -1,12 +1,12 @@
 defmodule AdventOfCode.Day07Test do
   use ExUnit.Case
 
-  import AdventOfCode.Day07
+  alias AdventOfCode.Day07
 
   @tag :skip
   test "part1" do
     input = nil
-    result = part1(input)
+    result = Day07.part1(input)
 
     assert result
   end
@@ -14,8 +14,37 @@ defmodule AdventOfCode.Day07Test do
   @tag :skip
   test "part2" do
     input = nil
-    result = part2(input)
+    result = Day07.part2(input)
 
     assert result
+  end
+
+  @tag :skip
+  test "format a bag with many types of bag inside" do
+    bag = "clear tan bags contain 5 bright purple bags, 1 pale black bag, 5 muted lime bags."
+    assert Day07.format_bag(bag) ==     %{
+        "clear tan" => %{
+            "bright purple" => 5,
+            "pale black" => 1,
+            "muted lime" => 5
+        }
+    }
+  end
+
+  @tag :skip
+  test "format a bag with just one type of bag inside" do
+    bag = "mirrored silver bags contain 4 wavy gray bags"
+    assert Day07.format_bag(bag) ==     %{
+        "mirrored silver" => %{
+            "wavy gray" => 4
+        }
+    }
+  end
+
+  test "format a bag with no content" do
+    bag = "dim tan bags contain no other bags"
+    assert Day07.format_bag(bag) ==     %{
+        "dim tan" => %{}
+    }
   end
 end
