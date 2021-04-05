be54fdaa4b58f4522d65502b7e7ae17d7825e4a6
Berenice Medel <bemesa21@gmail.com>
2021-04-05 03:30:41 -0500

    Format code

--- lib/advent_of_code/day_09.ex
+++ lib/advent_of_code/day_09.ex
@@ -6,7 +6,7 @@ defmodule AdventOfCode.Day09 do
   end
 
   def part2(input, preamble, n) do
-    formatted_input = format_input(input) 
+    formatted_input = format_input(input)
     num = find_the_wrong_number(formatted_input, preamble, n)
     result = search_contiguous_numbers(formatted_input, num)
     Enum.min(result) + Enum.max(result)
@@ -18,7 +18,7 @@ defmodule AdventOfCode.Day09 do
     |> Enum.map(&String.to_integer/1)
   end
 
-  def add_to?([hd | tail] = _numbers, n) do 
+  def add_to?([hd | tail] = _numbers, n) do
     Enum.reduce_while(tail, false, fn x, acc ->
       if hd + x != n, do: {:cont, false}, else: {:halt, true}
     end)
@@ -29,7 +29,7 @@ defmodule AdventOfCode.Day09 do
   def are_two_numbers_add_up_to_n?([hd | []], n), do: false
 
   def are_two_numbers_add_up_to_n?([_hd | tail] = numbers, n) do
-    if add_to?(numbers, n) do 
+    if add_to?(numbers, n) do
       true
     else
       are_two_numbers_add_up_to_n?(tail, n)
@@ -38,22 +38,24 @@ defmodule AdventOfCode.Day09 do
 
   def get_last_n_numbers(numbers, position, n) do
     if position < n do
-      {first, _ } = Enum.split(numbers,position)
+      {first, _} = Enum.split(numbers, position)
       first
-    else 
+    else
       Enum.slice(numbers, position - n, n)
     end
   end
 
   def find_the_wrong_number(numbers, preamble, n) do
-    positions = preamble .. length(numbers) - 2
-    first_number = Enum.at(numbers, preamble) 
+    positions = preamble..(length(numbers) - 2)
+    first_number = Enum.at(numbers, preamble)
+
     Enum.reduce_while(positions, first_number, fn x, acc ->
       last_25 = get_last_n_numbers(numbers, x - 1, n)
+
       if are_two_numbers_add_up_to_n?(last_25, acc) do
-         {:cont, Enum.at(numbers, x)}
+        {:cont, Enum.at(numbers, x)}
       else
-         {:halt, acc}
+        {:halt, acc}
       end
     end)
   end
@@ -62,6 +64,7 @@ defmodule AdventOfCode.Day09 do
 
   def contiguous_sum([hd | tail], {acc, num}) do
     sum = Enum.sum(acc) + hd
+
     cond do
       sum > num -> false
       sum < num -> contiguous_sum(tail, {[hd | acc], num})
@@ -73,10 +76,11 @@ defmodule AdventOfCode.Day09 do
 
   def search_contiguous_numbers([_hd | tail] = numbers, num) do
     result = contiguous_sum(numbers, {[], num})
+
     if result do
       result
     else
-      search_contiguous_numbers(tail,num)
+      search_contiguous_numbers(tail, num)
     end
   end
 end
--- test/advent_of_code/day_09_test.exs
+++ test/advent_of_code/day_09_test.exs
@@ -4,7 +4,9 @@ defmodule AdventOfCode.Day09Test do
   alias AdventOfCode.Day09
 
   test "part1: return 127" do
-    input = "35\n20\n15\n25\n47\n40\n62\n55\n65\n95\n102\n117\n150\n182\n127\n219\n299\n277\n309\n576"
+    input =
+      "35\n20\n15\n25\n47\n40\n62\n55\n65\n95\n102\n117\n150\n182\n127\n219\n299\n277\n309\n576"
+
     preamble = 5
     how_many_previous = 5
     result = Day09.part1(input, preamble, how_many_previous)
@@ -12,11 +14,13 @@ defmodule AdventOfCode.Day09Test do
   end
 
   test "part2" do
-    input = "35\n20\n15\n25\n47\n40\n62\n55\n65\n95\n102\n117\n150\n182\n127\n219\n299\n277\n309\n576"
+    input =
+      "35\n20\n15\n25\n47\n40\n62\n55\n65\n95\n102\n117\n150\n182\n127\n219\n299\n277\n309\n576"
+
     preamble = 5
     how_many_previous = 5
     result = Day09.part2(input, preamble, how_many_previous)
-    assert result == 62 
+    assert result == 62
   end
 
   test "format_input/1 should return a list of numbers" do
@@ -27,65 +31,177 @@ defmodule AdventOfCode.Day09Test do
     assert formatted_input == [35, 30]
   end
 
-
   test "return true if the first element of a list of integers plus another number of the list add to n " do
     n = 6
-    numbers = [1,2,3,4,5]
-    result = Day09.add_to?(numbers, n) 
+    numbers = [1, 2, 3, 4, 5]
+    result = Day09.add_to?(numbers, n)
     assert result == true
   end
 
   test "return false if the first element of a list of integers plus each of the rest of the numbers doesn't add to n" do
     n = 10
-    numbers = [1,2,3,4,5]
-    result = Day09.add_to?(numbers, n) 
+    numbers = [1, 2, 3, 4, 5]
+    result = Day09.add_to?(numbers, n)
     assert result == false
   end
 
-  test "return true if two of the elements of a list of integers add to n" do 
+  test "return true if two of the elements of a list of integers add to n" do
     n = 7
-    numbers = [1,2,3,4,5]
-    result = Day09.are_two_numbers_add_up_to_n?(numbers, n) 
+    numbers = [1, 2, 3, 4, 5]
+    result = Day09.are_two_numbers_add_up_to_n?(numbers, n)
     assert result == true
-  
   end
 
-  test "return false if none two elements of a list of integers add to n" do 
+  test "return false if none two elements of a list of integers add to n" do
     n = 100
-    numbers = [1,2,3,4,5]
-    result = Day09.are_two_numbers_add_up_to_n?(numbers, n) 
+    numbers = [1, 2, 3, 4, 5]
+    result = Day09.are_two_numbers_add_up_to_n?(numbers, n)
     assert result == false
   end
 
-  test "given a list of numbers and an existent position n < 25, should return numbers from pos 0 to n-1" do 
+  test "given a list of numbers and an existent position n < 25, should return numbers from pos 0 to n-1" do
     pos = 8
-    numbers = [1,2,3,4,5,6,7,8,9,10]
+    numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
     n = 25
-    result = Day09.get_last_n_numbers(numbers, pos, n) 
-    assert result == [1,2,3,4,5,6,7,8]
+    result = Day09.get_last_n_numbers(numbers, pos, n)
+    assert result == [1, 2, 3, 4, 5, 6, 7, 8]
   end
-  
 
-  test "given a list of numbers and a position=25, should return the fisrt 25 numbers" do 
+  test "given a list of numbers and a position=25, should return the fisrt 25 numbers" do
     pos = 25
-    numbers = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27]
+
+    numbers = [
+      1,
+      2,
+      3,
+      4,
+      5,
+      6,
+      7,
+      8,
+      9,
+      10,
+      11,
+      12,
+      13,
+      14,
+      15,
+      16,
+      17,
+      18,
+      19,
+      20,
+      21,
+      22,
+      23,
+      24,
+      25,
+      26,
+      27
+    ]
+
     n = 25
-    result = Day09.get_last_n_numbers(numbers, pos, n) 
+    result = Day09.get_last_n_numbers(numbers, pos, n)
     assert length(result) == 25
-    assert result == [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25]
+
+    assert result == [
+             1,
+             2,
+             3,
+             4,
+             5,
+             6,
+             7,
+             8,
+             9,
+             10,
+             11,
+             12,
+             13,
+             14,
+             15,
+             16,
+             17,
+             18,
+             19,
+             20,
+             21,
+             22,
+             23,
+             24,
+             25
+           ]
   end
-  
+
   test "given a list with length x >= 25 and a position > 25, should return the numbers in range n-25 - n-1" do
     pos = 27
-    numbers = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28]
+
+    numbers = [
+      1,
+      2,
+      3,
+      4,
+      5,
+      6,
+      7,
+      8,
+      9,
+      10,
+      11,
+      12,
+      13,
+      14,
+      15,
+      16,
+      17,
+      18,
+      19,
+      20,
+      21,
+      22,
+      23,
+      24,
+      25,
+      26,
+      27,
+      28
+    ]
+
     n = 25
-    result = Day09.get_last_n_numbers(numbers, pos, n) 
+    result = Day09.get_last_n_numbers(numbers, pos, n)
     assert length(result) == 25
-    assert result == [3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27]
-  end 
+
+    assert result == [
+             3,
+             4,
+             5,
+             6,
+             7,
+             8,
+             9,
+             10,
+             11,
+             12,
+             13,
+             14,
+             15,
+             16,
+             17,
+             18,
+             19,
+             20,
+             21,
+             22,
+             23,
+             24,
+             25,
+             26,
+             27
+           ]
+  end
 
   test "return 300, it isn't the sum of two numbers in the last 25 numbers starting from 5 preamble-numbers" do
-    numbers = [1,2,3,4,5,300]
+    numbers = [1, 2, 3, 4, 5, 300]
     preamble = 5
     n = 25
     result = Day09.find_the_wrong_number(numbers, preamble, n)
@@ -93,17 +209,17 @@ defmodule AdventOfCode.Day09Test do
   end
 
   test "if the first contiguous numbers of a list sum X returns a list with the first n numbers" do
-    numbers =  [2,2,2,1,5,6]
+    numbers = [2, 2, 2, 1, 5, 6]
     acc = []
     x = 7
     result = Day09.contiguous_sum(numbers, {acc, x})
-    assert Enum.reverse(result) == [2,2,2,1]
+    assert Enum.reverse(result) == [2, 2, 2, 1]
   end
 
   test "iterate until find contiguous numbers that sum 4, even if they are not at the start of the list" do
-    numbers =  [1,1,1,5,9,1,2,1]
+    numbers = [1, 1, 1, 5, 9, 1, 2, 1]
     x = 4
     result = Day09.search_contiguous_numbers(numbers, x)
-    assert Enum.reverse(result) == [1,2,1]
+    assert Enum.reverse(result) == [1, 2, 1]
   end
 end
