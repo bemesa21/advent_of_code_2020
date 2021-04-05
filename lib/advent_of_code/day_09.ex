fa3e4230c97bbc08741a4eade2c94d1381d79a1b
Berenice Medel <bemesa21@gmail.com>
2021-04-05 00:25:50 -0500

    Test if the first element of a list plus other of the numbers add to n [d09]

--- lib/advent_of_code/day_09.ex
+++ lib/advent_of_code/day_09.ex
@@ -11,5 +11,12 @@ defmodule AdventOfCode.Day09 do
     |> Enum.map(&String.to_integer/1)
   end
 
+  def add_to?([hd | tail] = _numbers, n) do 
+    Enum.reduce_while(tail, false, fn x, acc ->
+      if hd + x != n, do: {:cont, false}, else: {:halt, true}
+    end)
+  end
+
+
   
 end
--- test/advent_of_code/day_09_test.exs
+++ test/advent_of_code/day_09_test.exs
@@ -27,4 +27,18 @@ defmodule AdventOfCode.Day09Test do
     assert formatted_input == [35, 30]
   end
 
+
+  test "return true if the first element of a list of integers plus another number of the list add to n " do
+    n = 6
+    numbers = [1,2,3,4,5]
+    result = Day09.add_to?(numbers, n) 
+    assert result == true
+  end
+
+  test "return false if the first element of a list of integers plus each of the rest of the numbers doesn't add to n" do
+    n = 10
+    numbers = [1,2,3,4,5]
+    result = Day09.add_to?(numbers, n) 
+    assert result == false
+  end
 end
