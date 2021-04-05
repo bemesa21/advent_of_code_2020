6329c0270b659c4d66a4a7962715e9697a0f3c21
Berenice Medel <bemesa21@gmail.com>
2021-04-04 23:06:18 -0500

    Add a formatting input test

--- test/advent_of_code/day_09_test.exs
+++ test/advent_of_code/day_09_test.exs
@@ -1,12 +1,12 @@
 defmodule AdventOfCode.Day09Test do
   use ExUnit.Case
 
-  import AdventOfCode.Day09
+  alias AdventOfCode.Day09
 
   @tag :skip
   test "part1" do
     input = nil
-    result = part1(input)
+    result = Day09.part1(input)
 
     assert result
   end
@@ -14,8 +14,17 @@ defmodule AdventOfCode.Day09Test do
   @tag :skip
   test "part2" do
     input = nil
-    result = part2(input)
+    result = Day09.part2(input)
 
     assert result
   end
+
+  test "format_input/1 should return a list of numbers" do
+    input = "35\n30\n"
+    formatted_input = Day09.format_input(input)
+    assert is_list(formatted_input)
+    assert length(formatted_input) == 2
+    assert formatted_input == [35, 30]
+  end
+
 end
