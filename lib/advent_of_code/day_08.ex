b83cfd7bbad7ff9b127c7038b5d8d43d12e2a37b
Berenice Medel <bemesa21@gmail.com>
2021-04-06 16:32:16 -0500

    Add first part and tests [d08]

--- lib/advent_of_code/day_08.ex
+++ lib/advent_of_code/day_08.ex
@@ -1,7 +1,67 @@
 defmodule AdventOfCode.Day08 do
-  def part1(args) do
+  def part1(input) do
+    input
+    |> format_input()
+    |> IO.inspect(label: :formatted_input)
+    |> process_program(0, 0)
   end
 
   def part2(args) do
+  end
+
+  def format_input(input) do
+    input
+    |> String.split("\n", trim: true)
+    |> Enum.with_index()
+    |> Enum.reduce(%{}, fn {instruction, index}, acc ->
+      formatted_instruction = format_instruction(instruction)
+      Map.put(acc, index, formatted_instruction)
+    end)
+  end
+
+  def format_instruction(instruction) do
+    case instruction do
+      "jmp " <> num ->
+        %{
+          instruction: "jmp",
+          number: String.to_integer(num),
+          visited?: false
+        }
+
+      "acc " <> num ->
+        %{
+          instruction: "acc",
+          number: String.to_integer(num),
+          visited?: false
+        }
+
+      _ ->
+        %{
+          instruction: "nop",
+          visited?: false
+        }
+    end
+  end
+
+  def process_program(instructions, counter, accumulator) do
+    instruction = instructions[counter]
+
+    if(is_nil(instruction) or instruction.visited?) do
+      accumulator
+    else
+      case instruction do
+        %{instruction: "acc", number: number} ->
+          instructions = Map.put(instructions, counter, %{instruction | visited?: true})
+          process_program(instructions, counter + 1, accumulator + number)
+
+        %{instruction: "jmp", number: number} ->
+          instructions = Map.put(instructions, counter, %{instruction | visited?: true})
+          process_program(instructions, counter + number, accumulator)
+
+        %{instruction: "nop"} ->
+          instructions = Map.put(instructions, counter, %{instruction | visited?: true})
+          process_program(instructions, counter + 1, accumulator)
+      end
+    end
   end
 end
--- test/advent_of_code/day_08_test.exs
+++ test/advent_of_code/day_08_test.exs
@@ -1,21 +1,82 @@
 defmodule AdventOfCode.Day08Test do
   use ExUnit.Case
 
-  import AdventOfCode.Day08
+  alias AdventOfCode.Day08
 
-  @tag :skip
   test "part1" do
-    input = nil
-    result = part1(input)
+    input = "nop +0\nacc +1\njmp +4\nacc +3\njmp -3\nacc -99\nacc +1\njmp -4\nacc +6"
+    result = Day08.part1(input)
 
-    assert result
+    assert result == 5
   end
 
   @tag :skip
   test "part2" do
     input = nil
-    result = part2(input)
+    result = Day08.part2(input)
 
     assert result
+  end
+
+  test "format a instruction jmp should return a map with three elements" do
+    result = Day08.format_instruction("jmp -3")
+
+    assert result == %{
+             instruction: "jmp",
+             number: -3,
+             visited?: false
+           }
+  end
+
+  test "format a instruction acc should return a map with three elements" do
+    result = Day08.format_instruction("acc -3")
+
+    assert result == %{
+             instruction: "acc",
+             number: -3,
+             visited?: false
+           }
+  end
+
+  test "format a instruction nop should return a map with three elements" do
+    result = Day08.format_instruction("nop -0")
+
+    assert result == %{
+             instruction: "nop",
+             visited?: false
+           }
+  end
+
+  test "format a string of instructions should return an indexed map" do
+    result = Day08.format_input("acc -3\nnop -0 ")
+
+    assert result ==
+             %{
+               0 => %{
+                 instruction: "acc",
+                 number: -3,
+                 visited?: false
+               },
+               1 => %{
+                 instruction: "nop",
+                 visited?: false
+               }
+             }
+  end
+
+  test "A program with just an acc +3 instruction should return 3" do
+    result =
+      Day08.format_input("acc -3")
+      |> Day08.process_program(0, 0)
+
+    assert result == -3
+  end
+
+  test "A program with just an nop instruction should return 1" do
+    result =
+      Day08.format_input("nop + 0")
+      |> Day08.process_program(0, 0)
+
+    assert result == 0
   end
 end
