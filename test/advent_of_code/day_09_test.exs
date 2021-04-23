ac085ea6088869bcb9e9fb674c56f0a960067ffb
Berenice Medel <bemesa21@gmail.com>
2021-04-22 20:15:19 -0500

    Remove unnecessary variables in tests [d09]

--- test/advent_of_code/day_09_test.exs
+++ test/advent_of_code/day_09_test.exs
@@ -9,8 +9,7 @@ defmodule AdventOfCode.Day09Test do
 
     preamble = 5
     how_many_previous = 5
-    result = Day09.part1(input, preamble, how_many_previous)
-    assert result == 127
+    assert Day09.part1(input, preamble, how_many_previous) == 127
   end
 
   test "part2" do
@@ -19,52 +18,43 @@ defmodule AdventOfCode.Day09Test do
 
     preamble = 5
     how_many_previous = 5
-    result = Day09.part2(input, preamble, how_many_previous)
-    assert result == 62
+    assert Day09.part2(input, preamble, how_many_previous) == 62
   end
 
   test "format_input/1 should return a list of numbers" do
     input = "35\n30\n"
-    formatted_input = Day09.format_input(input)
-    assert is_list(formatted_input)
-    assert length(formatted_input) == 2
-    assert formatted_input == [35, 30]
+    assert Day09.format_input(input) == [35, 30]
   end
 
   test "return true if the first element of a list of integers plus another number of the list add to n " do
     n = 6
     numbers = [1, 2, 3, 4, 5]
-    result = Day09.add_to?(numbers, n)
-    assert result == true
+    assert Day09.add_to?(numbers, n) == true
   end
 
   test "return false if the first element of a list of integers plus each of the rest of the numbers doesn't add to n" do
     n = 10
     numbers = [1, 2, 3, 4, 5]
-    result = Day09.add_to?(numbers, n)
-    assert result == false
+    assert Day09.add_to?(numbers, n) == false
   end
 
   test "return true if two of the elements of a list of integers add to n" do
     n = 7
     numbers = [1, 2, 3, 4, 5]
-    result = Day09.are_two_numbers_add_up_to_n?(numbers, n)
-    assert result == true
+    assert Day09.are_two_numbers_add_up_to_n?(numbers, n) == true
   end
 
   test "return false if none two elements of a list of integers add to n" do
     n = 100
     numbers = [1, 2, 3, 4, 5]
-    result = Day09.are_two_numbers_add_up_to_n?(numbers, n)
-    assert result == false
+    assert Day09.are_two_numbers_add_up_to_n?(numbers, n) == false
   end
 
   test "given a list of numbers and an existent position n < 25, should return numbers from pos 0 to n-1" do
     pos = 8
     numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
     n = 25
-    result = Day09.get_last_n_numbers(numbers, pos, n)
-    assert result == [1, 2, 3, 4, 5, 6, 7, 8]
+    assert  Day09.get_last_n_numbers(numbers, pos, n) == [1, 2, 3, 4, 5, 6, 7, 8]
   end
 
   test "given a list of numbers and a position=25, should return the fisrt 25 numbers" do
@@ -204,8 +194,7 @@ defmodule AdventOfCode.Day09Test do
     numbers = [1, 2, 3, 4, 5, 300]
     preamble = 5
     n = 25
-    result = Day09.find_the_wrong_number(numbers, preamble, n)
-    assert result == 300
+    assert Day09.find_the_wrong_number(numbers, preamble, n) == 300
   end
 
   test "if the first contiguous numbers of a list sum X returns a list with the first n numbers" do
