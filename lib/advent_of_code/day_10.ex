b633dfd025a1570407e3ab625d4d0a8315332b7f
Berenice Medel <bemesa21@gmail.com>
2021-04-27 12:44:57 -0500

    Add part 1[d10]

--- lib/advent_of_code/day_10.ex
+++ lib/advent_of_code/day_10.ex
@@ -1,7 +1,82 @@
 defmodule AdventOfCode.Day10 do
-  def part1(args) do
+  def part1(input) do
+    differences =
+      input
+      |> format_input()
+      |> Enum.sort()
+      #|> IO.inspect(label: :input)
+      |> construct_adapter()
+      #|> IO.inspect(label: :adapter)
+      |> find_differences()
+      #|> IO.inspect(label: :differences)
+      |> group_differences()
+
+    differences[1] * differences[3]
   end
 
   def part2(args) do
+  end
+
+  def format_input(input) do
+    input
+    |> String.split("\n", trim: true)
+    |> Enum.map(&String.to_integer/1)
+  end
+
+  def is_valid_difference?(num1, num2) when num1 == num2 do
+    false
+  end
+
+  def is_valid_difference?(num1, num2) do
+    (num1 - num2) in -3..3
+  end
+
+  def construct_adapter([add1] = _adapters), do: [add1]
+
+  def construct_adapter([add1, add2] = _adapters) do
+    if is_valid_difference?(add1, add2) do
+      [add1, add2]
+    else
+      false
+    end
+  end
+
+  def construct_adapter([hd | tail] = _adapters) do
+    valid_options = Enum.filter(tail, &is_valid_difference?(hd, &1))
+    posible = posible_combinations(valid_options, tail)
+
+    result =
+      Enum.reduce_while(posible, [], fn {key, [x]}, _acc ->
+        result = construct_adapter([key | x])
+        if result == false, do: {:cont, []}, else: {:halt, result}
+      end)
+
+    [hd | result]
+  end
+
+  def posible_combinations(valid_options, adapters) do
+    Enum.group_by(valid_options, & &1, &Enum.reject(adapters, fn x -> &1 == x end))
+  end
+
+  def find_differences(jolts) do
+    jolts = jolts ++ [List.last(jolts) + 3]
+
+    {_final, differences} =
+      Enum.reduce(jolts, {0, []}, fn x, {prev, acc} ->
+        difference = x - prev
+        {x, [difference | acc]}
+      end)
+
+    differences
+  end
+
+  def group_differences(differences) do
+    Enum.reduce(differences, %{}, fn x, acc ->
+      if Map.has_key?(acc, x) do
+        Map.put(acc, x, acc[x] + 1)
+      else
+        Map.put_new(acc, x, 1)
+      end
+    end)
   end
 end
--- test/advent_of_code/day_10_test.exs
+++ test/advent_of_code/day_10_test.exs
@@ -1,21 +1,103 @@
 defmodule AdventOfCode.Day10Test do
   use ExUnit.Case
 
-  import AdventOfCode.Day10
+  alias AdventOfCode.Day10
 
-  @tag :skip
-  test "part1" do
-    input = nil
-    result = part1(input)
+  test "part 1: Test tiny example input" do
+    input = """
+    16
+    10
+    15
+    5
+    1
+    11
+    7
+    19
+    6
+    12
+    4
+    """
 
-    assert result
+    assert Day10.part1(input) == 35
+  end
+
+  test "Part1: Test medium example input" do
+    input = """
+    28
+    33
+    18
+    42
+    31
+    14
+    46
+    20
+    48
+    47
+    24
+    23
+    49
+    45
+    19
+    38
+    39
+    11
+    1
+    32
+    25
+    35
+    8
+    17
+    7
+    9
+    4
+    2
+    34
+    10
+    3
+    """
+
+    assert Day10.part1(input) == 220
   end
 
   @tag :skip
   test "part2" do
     input = nil
-    result = part2(input)
+    result = Day10.part2(input)
 
     assert result
+  end
+
+  test "the difference between 4 and 2 is valid" do
+    assert Day10.is_valid_difference?(4, 2) == true
+  end
+
+  test "the difference between 4 and 1 is valid" do
+    assert Day10.is_valid_difference?(4, 1) == true
+  end
+
+  test "the difference between 1 and 5 is invalid" do
+    assert Day10.is_valid_difference?(1, 5) == false
+  end
+
+  test "differences has to be [1, 3]" do
+    result =
+      [1]
+      |> Day10.find_differences()
+      |> Enum.reverse()
+
+    assert result == [1, 3]
+  end
+
+  test "differences for small input has to be [1, 3, 1, 1, 1, 3, 1, 1, 3, 1, 3, 3]" do
+    result =
+      [1, 4, 5, 6, 7, 10, 11, 12, 15, 16, 19]
+      |> Day10.find_differences()
+      |> Enum.reverse()
+
+    assert result == [1, 3, 1, 1, 1, 3, 1, 1, 3, 1, 3, 3]
+  end
+
+  test "has to return a 7 difference of one and 5 differences of 5" do
+    assert Day10.group_differences([1, 3, 1, 1, 1, 3, 1, 1, 3, 1, 3, 3]) == %{1 => 7, 3 => 5}
   end
 end
