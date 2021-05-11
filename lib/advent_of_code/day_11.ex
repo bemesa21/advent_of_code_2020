912c40d3dcafad926f920f9ef54907109474da4b
Berenice Medel <bemesa21@gmail.com>
2021-05-11 12:45:36 -0500

    Add part1 and tests [d11]

--- lib/advent_of_code/day_11.ex
+++ lib/advent_of_code/day_11.ex
@@ -1,7 +1,114 @@
 defmodule AdventOfCode.Day11 do
-  def part1(args) do
+  @positions [
+    {-1, -1},
+    {-1, 0},
+    {-1, 1},
+    {0, -1},
+    {0, 1},
+    {1, -1},
+    {1, 0},
+    {1, 1}
+  ]
+  def part1(input) do
+    result =
+    input
+    |> format_input()
+    |> find_the_best_position()
+    |> seats_list()
+    |> group_seats()
+
+    result["#"]
   end
 
   def part2(args) do
+  end
+
+  def format_input(input) do
+    input
+    |> String.split("\n", trim: true)
+    |> Enum.with_index()
+    |> Enum.reduce(%{}, &construct_multi_map/2)
+  end
+
+  defp construct_multi_map({line, index}, acc) do
+    result =
+      line
+      |> String.codepoints()
+      |> Enum.with_index()
+      |> Enum.reduce(%{}, &to_map/2)
+
+    to_map({result, index}, acc)
+  end
+
+  defp to_map({value, index}, acc) do
+    Map.put_new(acc, index, value)
+  end
+
+  def dimensions(map) do
+    x = map |> Map.keys() |> Enum.max()
+    y = map[0] |> Map.keys() |> Enum.max()
+    {x, y}
+  end
+
+  def get_adjacencies(seats, current_position) do
+    seats
+    |> get_adjacent_seats(current_position)
+    |> group_seats()
+  end
+
+  defp get_adjacent_seats(seats, {x, y}) do
+    @positions
+    |> Enum.reduce([], fn {dif_x, dif_y}, acc ->
+      [seats[x + dif_x][y + dif_y] | acc]
+    end)
+    |> Enum.filter(& &1)
+  end
+
+  defp group_seats(adjacent_seats) do
+    adjacent_seats
+    |> Enum.reduce(%{}, fn x, acc ->
+      Map.update(acc, x, 1, &(&1 + 1))
+    end)
+  end
+
+  def apply_rules(seats) do
+    Enum.reduce(seats, seats, fn {x, values}, acc ->
+      Enum.reduce(values, acc, fn {y, seat}, acc2 ->
+        adjacent_seats = get_adjacencies(seats, {x, y})
+        put_in(acc2[x][y], apply_rule(seat, adjacent_seats))
+      end)
+    end)
+  end
+
+  def apply_rule("L", %{"#" => _}), do: "L"
+  def apply_rule("L", _), do: "#"
+
+  def apply_rule("#", %{"#" => num}) when num >= 4, do: "L"
+  def apply_rule("#", _), do: "#"
+
+  def apply_rule(other, _), do: other
+
+  def to_string(seats) do
+    Enum.reduce(seats, "", fn {_key, x}, acc ->
+      r = Enum.map_join(x, fn {_key, value} -> value end)
+      "#{acc}#{r}\n"
+    end)
+  end
+
+  def find_the_best_position(seats) do
+    result = apply_rules(seats)
+    if seats == result do
+      result
+    else
+      find_the_best_position(result)
+    end
+  end
+
+  defp seats_list(seats) do
+    Enum.reduce(seats, [], fn {_k1,v1}, acc1 ->
+      Enum.reduce(v1, acc1, fn {_k2, value}, acc2 ->
+        [value | acc2]
+      end)
+    end)
   end
 end
--- test/advent_of_code/day_11_test.exs
+++ test/advent_of_code/day_11_test.exs
@@ -1,21 +1,194 @@
 defmodule AdventOfCode.Day11Test do
   use ExUnit.Case
 
-  import AdventOfCode.Day11
+  alias AdventOfCode.Day11
 
-  @tag :skip
   test "part1" do
-    input = nil
-    result = part1(input)
+    input = """
+    L.LL.LL.LL
+    LLLLLLL.LL
+    L.L.L..L..
+    LLLL.LL.LL
+    L.LL.LL.LL
+    L.LLLLL.LL
+    ..L.L.....
+    LLLLLLLLLL
+    L.LLLLLL.L
+    L.LLLLL.LL
+    """
+    assert Day11.part1(input) == 37
 
-    assert result
   end
 
   @tag :skip
   test "part2" do
     input = nil
-    result = part2(input)
+    result = Day11.part2(input)
 
     assert result
+  end
+
+  test "format an input with one line has to return a map with just one key" do
+    input = "L.LL.LL.L\n"
+
+    assert %{
+             0 => %{
+               0 => "L",
+               1 => ".",
+               2 => "L",
+               3 => "L",
+               4 => ".",
+               5 => "L",
+               6 => "L",
+               7 => ".",
+               8 => "L"
+             }
+           } == Day11.format_input(input)
+  end
+
+  test "dimensions/1 returns {0, 8}" do
+    input = %{
+      0 => %{
+        0 => "L",
+        1 => ".",
+        2 => "L",
+        3 => "L",
+        4 => ".",
+        5 => "L",
+        6 => "L",
+        7 => ".",
+        8 => "L"
+      }
+    }
+
+    assert {0, 8} == Day11.dimensions(input)
+  end
+
+  test "get_adjacencies/2 returns a map with 3#, 4L, 1." do
+    input = %{
+      0 => %{
+        0 => "L",
+        1 => "#",
+        2 => "."
+      },
+      1 => %{
+        0 => "L",
+        1 => "*",
+        2 => "L"
+      },
+      2 => %{
+        0 => "#",
+        1 => "L",
+        2 => "#"
+      }
+    }
+
+    result = Day11.get_adjacencies(input, {1, 1})
+    assert result["#"] == 3
+    assert result["L"] == 4
+    assert result["."] == 1
+  end
+
+  test "get_adjacencies/2 in the upside frontier" do
+    input = %{
+      0 => %{
+        0 => "L",
+        1 => "#",
+        2 => "."
+      },
+      1 => %{
+        0 => "L",
+        1 => "*",
+        2 => "L"
+      },
+      2 => %{
+        0 => "#",
+        1 => "L",
+        2 => "#"
+      }
+    }
+
+    result = Day11.get_adjacencies(input, {0, 1})
+    assert result["*"] == 1
+    assert result["L"] == 3
+    assert result["."] == 1
+  end
+
+  test "If a seat is empty (L) and there are no occupied seats adjacent to it, the seat becomes occupied #." do
+    assert Day11.apply_rule("L", %{"L" => 1}) == "#"
+  end
+
+  test "If a seat is empty (L) and there occupied seats adjacent to it, the seat doesnt change" do
+    assert Day11.apply_rule("L", %{"L" => 1, "#" => 1}) == "L"
+  end
+
+  test "If a seat is occupied (#) and four or more seats adjacent to it are also occupied, the seat becomes empty" do
+    assert Day11.apply_rule("#", %{"#" => 4}) == "L"
+  end
+
+  test "If a seat is occupied (#) and four or more seats adjacent to it are also occupied, it doesnt change" do
+    assert Day11.apply_rule("#", %{"L" => 1, "#" => 1}) == "#"
+  end
+
+  test "apply_rules/1 with example input, round1" do
+    input = """
+    L.LL.LL.LL
+    LLLLLLL.LL
+    L.L.L..L..
+    LLLL.LL.LL
+    L.LL.LL.LL
+    L.LLLLL.LL
+    ..L.L.....
+    LLLLLLLLLL
+    L.LLLLLL.L
+    L.LLLLL.LL
+    """
+
+    result = input |> Day11.format_input() |> Day11.apply_rules() |> Day11.to_string()
+
+    assert result ==
+             """
+             #.##.##.##
+             #######.##
+             #.#.#..#..
+             ####.##.##
+             #.##.##.##
+             #.#####.##
+             ..#.#.....
+             ##########
+             #.######.#
+             #.#####.##
+             """
+  end
+
+  test "apply_rules/1 with example input, round2" do
+    input = """
+    #.##.##.##
+    #######.##
+    #.#.#..#..
+    ####.##.##
+    #.##.##.##
+    #.#####.##
+    ..#.#.....
+    ##########
+    #.######.#
+    #.#####.##
+    """
+
+    result = input |> Day11.format_input() |> Day11.apply_rules() |> Day11.to_string()
+
+    assert result ==
+             """
+             #.LL.L#.##
+             #LLLLLL.L#
+             L.L.L..L..
+             #LLL.LL.L#
+             #.LL.LL.LL
+             #.LLLL#.##
+             ..L.L.....
+             #LLLLLLLL#
+             #.LLLLLL.L
+             #.#LLLL.##
+             """
   end
 end
